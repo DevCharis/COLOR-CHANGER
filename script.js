6cb7c88c31364441b6cf016458ef1936a7c5713e
DevCharis <joshuabamidele9@gmail.com>
2025-10-28 18:45:23 +0100

    new file:   index.html 	new file:   new.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('red').addEventListener('click', () => {
+  document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('blue').addEventListener('click', () => {
+  document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('green').addEventListener('click', () => {
+  document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+  document.body.style.backgroundColor = 'white';
+});
+document.getElementById("grey").addEventListener('click', () =>{
+  document.body.style.backgroundColor = "grey";
+})
